11ae1530a7c1c71172c96c6a73cd523ad8118a65
Anish Shukla <anishshukla88@gmail.com>
2014-12-23 17:36:04 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='log-watcher',
+      version='1.0',
+      description='Log watcher to verify logs',
+      author='Anish Shukla',
+      author_email='shuklaanish@rocketmail.com',
+      url='https://github.com/anishShukla/log-watcher/',
+      packages=['log-watcher'],
+     )
